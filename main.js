8754beec47167d50ae7aa262b4f40b419a3a6469
Bahodir <rbaxa.br@gmail.com>
2025-02-10 16:56:53 +0500

    v2

--- main.js
+++ main.js
@@ -1,47 +1,57 @@
-$(document).ready(function(){
+$(document).ready(function () {
+	// Function to display current time
+	function currentTime() {
+		let data = new Date();
+		let hour = data.getHours();
+		let min = data.getMinutes();
+		let secon = data.getSeconds();
 
-    function currentTime() {
-        let data = new Date();
-    let hour = data.getHours();
-    let secon = data.getSeconds()
-    let min = data.getMinutes();
-  
+		// Add leading zeros if necessary
+		min = min < 10 ? '0' + min : min;
+		hour = hour < 10 ? '0' + hour : hour;
+		secon = secon < 10 ? '0' + secon : secon;
 
-    (min < 10) ? min = '0'+ min : min ;
-    (hour < 10) ? hour = '0'+ hour : hour;
-    (secon < 10) ? secon = '0' + secon : secon ;
+		// Format the time
+		let showTime = hour + ':' + min + ':' + secon;
+		$('.time').html(showTime);
+	}
 
-    let showTime = hour + ':' + min + ':' + secon;
+	// Call currentTime() initially and then every second
+	currentTime();
+	setInterval(currentTime, 1000);
 
-    $('.time').html(showTime)
-        
-    }
-    setInterval(currentTime,1000)
-    
-    
-    function currentDate() {
-        let data = new Date();
-    let dd = data.getDate()
-    let mm = data.getMonth() +1
-    let yy = data.getFullYear()
-    console.log(data.getMonth());
+	// Function to display current date
+	function currentDate() {
+		let data = new Date();
+		let dd = data.getDate();
+		let mm = data.getMonth() + 1; // Months are zero-based
+		let yy = data.getFullYear();
 
-     (dd < 10) ? dd = '0'+ dd : dd;
-     (mm < 10) ? mm = '0' + mm : mm;
-     (yy < 10) ? yy = '0' + yy : yy;
+		// Add leading zeros if necessary
+		dd = dd < 10 ? '0' + dd : dd;
+		mm = mm < 10 ? '0' + mm : mm;
 
-    let showDate = dd +':'+ mm + ':' + yy
-    $('.date').html(showDate)
-    }
-    currentDate();
-let d = 270
-    function roundClock() {
-        let data = new Date();
-        let audio = new Audio('tic.mp3')
-        let secon = data.getSeconds()
-        $('.strelka').css({'transform':'rotate('+ secon *6+  'deg)'})
-        // audio.play()
-    }
-    setInterval(roundClock,1000)
-    
-})
+		// Format the date
+		let showDate = dd + '/' + mm + '/' + yy;
+		$('.date').html(showDate);
+	}
+
+	// Call currentDate() initially
+	currentDate();
+
+	// Function to rotate the clock hand
+	function roundClock() {
+		let data = new Date();
+		let secon = data.getSeconds();
+		let audio = new Audio('tic.mp3'); // Ensure 'tic.mp3' exists in the correct directory
+
+		// Rotate the clock hand based on the current second
+		$('.strelka').css({ transform: 'rotate(' + secon * 6 + 'deg)' });
+
+		// Play the tick sound (uncomment if you want the sound)
+		audio.play();
+	}
+
+	// Call roundClock() every second
+	setInterval(roundClock, 1000);
+});
--- timer.js
+++ timer.js
@@ -1,62 +1,49 @@
-$(document).ready(function(){
+$(document).ready(function () {
+	let hh = 0;
+	let mm = 0;
+	let ss = 0;
 
-    let hh = 0;
-    let mm = 0;
-    let ss = 0;
+	$('.content__timer').html(hh + ':' + mm + ':' + ss);
 
+	let isPaused = false;
 
-    $('.content__timer').html(hh +':' + mm + ':' + ss)
+	function playTimer() {
+		mm < 10 ? (mm = '0' + Number(mm)) : mm;
+		hh < 10 ? (hh = '0' + Number(hh)) : hh;
+		ss < 10 ? (ss = '0' + Number(ss)) : ss;
 
-    let isPaused = false;
+		if (isPaused) {
+			ss++;
+		}
+		$('.content__timer').html(hh + ':' + mm + ':' + ss);
 
-    function playTimer() {
-        
-        (mm < 10) ? mm = '0'+ Number(mm) : mm;
-       (hh < 10) ? hh = '0'+ Number(hh) :hh ;
-     (ss < 10) ? ss = '0' + Number(ss) : ss ;
-        
-        if (isPaused === false) {
-            ss==ss;
-        }else{
-            ss ++
-        }
-        $('.content__timer').html(hh +':' + mm + ':' + ss)
-        
-        if (ss == 100) {
-            ss = 0;
-            mm ++;
-        } 
-        if (mm == 60) {
-            mm = 0;
-            hh ++;
-        }
-        
-        
-    }
+		if (ss == 100) {
+			ss = 0;
+			mm++;
+		}
+		if (mm == 60) {
+			mm = 0;
+			hh++;
+		}
+	}
 
-   
+	let start = $('.s');
+	start.click(function (e) {
+		e.preventDefault();
+		isPaused = true;
+	});
 
-    let start = $('.s')
-    start.click(function (e) { 
-        e.preventDefault();
-        isPaused = true;
-        
-    });
+	$('.p').click(function (e) {
+		e.preventDefault();
 
-    $('.p').click(function(e){
-        e.preventDefault()
+		isPaused = false;
+	});
 
-        isPaused = false;
-    })
+	$('.r').click((e) => {
+		e.preventDefault();
+		hh = ss = mm = 0;
+		isPaused = false;
+	});
 
-    $('.r').click(e => {
-        e.preventDefault()
-        hh=ss=mm=0;
-        isPaused = false
-    })
-
-    
-
-setInterval(playTimer,10)
-    
-})
+	setInterval(playTimer, 10);
+});
